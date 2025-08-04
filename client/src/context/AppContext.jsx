6e96bcd538a216ab06540bb1a6e38791fcd3d401
Mwosa <nelsonmwosa7@gmail.com>
2025-08-04 14:38:45 +0300

    Fixed order placement

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export const AppContext = createContext();
 
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -59,7 +59,7 @@ const Cart = () => {
       // Place order with COD 
       if(paymentOption === "COD") {
         const {data} = await axios.post("/api/order/cod", {
-          userId: user._id,
+          // userId: user._id,
           items: cartArray.map(item => ({product: item._id, quantity: item.quantity})),
           address: selectedAddress._id
         })
@@ -74,7 +74,7 @@ const Cart = () => {
       } else {
         // Place order with Stripe
         const {data} = await axios.post("/api/order/stripe", {
-          userId: user._id,
+          // userId: user._id,
           items: cartArray.map(item => ({product: item._id, quantity: item.quantity})),
           address: selectedAddress._id
         })
